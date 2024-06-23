ed592454549c9b713065b4278059b6ce7eeb7401
Theo Ojochegbe <symplytheo@gmail.com>
2024-06-23 15:06:59 +0100

    chore: update error message selection

--- src/redux/middlewares/errorMiddleware.js
+++ src/redux/middlewares/errorMiddleware.js
@@ -4,22 +4,21 @@ const errorMiddleware = () => (next) => (action) => {
   const isRejected = action.type.endsWith("/rejected");
   const isFulfilled = action.type.endsWith("/fulfilled");
 
-  // check for error message
-  const errorMessage =
-    action.payload?.data?.error ||
-    action.payload?.data?.message ||
-    action.error?.message ||
-    action?.error ||
-    action.payload?.error ||
-    "Oops, something went wrong!";
+  const payload = action.payload || {};
+  const { data, error, message } = payload;
+  const errorMessage = data
+    ? Array.isArray(data?.message)
+      ? data?.message?.[0]
+      : Array.isArray(data?.error)
+        ? data?.error?.[0]
+        : data?.message || data?.error
+    : message || error || "Oops, something went wrong!";
 
-  // check if action is rejected or is fufilled but an error exists
-  if ((isFulfilled && action.payload?.error) || isRejected) {
-    // Use toast.error to show a toast with the error message
+  console.log("err", errorMessage);
+  if ((isFulfilled && error) || isRejected) {
     toast.error(errorMessage);
   }
 
-  // Pass the action to the next middleware or the reducer
   return next(action);
 };
 
